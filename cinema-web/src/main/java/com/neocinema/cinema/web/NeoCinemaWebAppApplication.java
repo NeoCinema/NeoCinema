c1d64e223e91c76f419871282aa7b384524075a0
JoseC8a <167725372+JoseC8a@users.noreply.github.com>
2024-05-21 10:17:07 -0700

    Add files via upload
    Base del proyecto V1 en blanco con todos los repositorios necesarios

---
+++ cinema-web/src/main/java/com/neocinema/cinema/web/NeoCinemaWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.neocinema.cinema.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoCinemaWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoCinemaWebAppApplication.class, args);
+	}
+
+}
---
+++ cinema-web/src/test/java/com/neocinema/cinema/web/NeoCinemaWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neocinema.cinema.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoCinemaWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
